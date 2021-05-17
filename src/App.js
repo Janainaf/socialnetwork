cc851a2aa3103a699d6e06d5f621db75912281e6
Janainaf <figueira.janaina@gmail.com>
2021-05-17 19:06:27 +0200

    redux and redux react installation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Ouxi <code>src/App.js</code> cde?
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          nnqo auero
         </a>
       </header>
     </div>
